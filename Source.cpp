7edc29a341e4b3adf5219e5ca8748684e99b5af7
JuliusNarvilas <julius.narvilas@gmail.com>
2018-11-11 23:19:09 +0000

    test

--- Source.cpp
+++ Source.cpp
@@ -82,7 +82,7 @@ constexpr uint64 swapByteOrderOld(uint64 ull)
 }
 */
 
-constexpr uint64 swapByteOrder(uint64 val)
+uint64 swapByteOrder(uint64 val)
 {
 	val = ((val << 8) & 0xFF00FF00FF00FF00ULL) | ((val >> 8) & 0x00FF00FF00FF00FFULL);
 	val = ((val << 16) & 0xFFFF0000FFFF0000ULL) | ((val >> 16) & 0x0000FFFF0000FFFFULL);
